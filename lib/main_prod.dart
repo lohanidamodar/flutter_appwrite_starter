2b25cd91c6fd3a6d5f8232543568ce128588d67f
Damodar Lohani <dlohani48@gmail.com>
2021-08-11 18:57:18 +0545

    fix production main

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,11 +1,18 @@
+import 'package:flappwrite_account_kit/flappwrite_account_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_appwrite_starter/app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'core/data/service/api_service.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Crashlytics.instance.enableInDevMode = true;
-    runApp(
-      ProviderScope(child: App()),
-    );
+  runApp(
+    FlAppwriteAccountKit(
+      client: ApiService.instance.client,
+      child: ProviderScope(
+        child: App(),
+      ),
+    ),
+  );
 }
