73261f67dfebe1b28d2f824bdb3a6b78d0d40e7c
Damodar Lohani <dlohani48@gmail.com>
2023-05-14 07:31:19 +0545

    enable macos project

--- lib/core/res/constants.dart
+++ lib/core/res/constants.dart
@@ -2,6 +2,6 @@ class AppConstants {
   static const String appName = "Appwrite starter";
   static const String appNameDev = "[DEV] Appwrite starter";
   static const String endpoint = "https://demo.appwrite.io/v1";
-  static const String projectId = "606e4205b3b5c";
+  static const String projectId = "appwrite-starter";
   static const String profileBucketId = 'profile_pics';
 }
