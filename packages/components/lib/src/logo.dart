458682d2f48f5dad8a7ac52ae14f0706e593c6f3
Damodar Lohani <dlohani48@gmail.com>
2023-06-12 12:49:43 +0545

    fix asset not loading

--- packages/components/lib/src/logo.dart
+++ packages/components/lib/src/logo.dart
@@ -10,7 +10,7 @@ class Logo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SvgPicture.asset(
-      Assets.logo,
+      'packages/components/${Assets.logo}',
       height: height,
     );
   }
